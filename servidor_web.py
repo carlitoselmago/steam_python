dae2b5cdcde95dfb60b56059d57d27c3812f72e7
carlitoselmago <ccarbonell@gmail.com>
2025-09-05 14:47:38 +0200

    changes

--- servidor_web.py
+++ servidor_web.py
@@ -15,8 +15,9 @@ def home():
 	HTML+='<link rel= "stylesheet" type= "text/css" href= "static/main.css">'
 	HTML+="<div style='display: flex;flex-direction: column;align-items: flex-start;'>"
 	HTML+="<h1>Google trucho</h1>"
-	HTML+="<form>"
-	HTML+="<input name='search'>"
+	
+	HTML+="<form action='search' method='GET'>"
+	HTML+="<input name='s'>"
 	HTML+="<input type='submit'>"
 	HTML+="</form>"
 
@@ -32,6 +33,34 @@ def home():
 			HTML+='</div>'
 
 		return HTML
+
+@app.route("/search",methods = ['GET'])
+def loginProcess():
+
+	connection = pymysql.connect(host='192.168.1.79',
+							user='remoto',
+							password='12345',
+							database='steam',
+							cursorclass=pymysql.cursors.DictCursor)
+	
+	cursor= connection.cursor()
+	s = request.args.get('s')
+	sql = 'SELECT * FROM steam.	apps WHERE name LIKE "%'+s+'%"'
+	print(sql)
+	#l'executem
+	cursor.execute(sql)
+	#atrapem els resultats, podem rebre tots o nomes un amb fetchone()
+	resultats = cursor.fetchall()
+	
+	HTML=''
+	for app in resultats:
+		HTML+='<img src="https://cdn.cloudflare.steamstatic.com/steam/apps/'+str(app["appid"])+'/header.jpg" style="border-radius:12px">'
+		HTML+='<h3 class="titulo">'+app["name"]+'</h3>'
+
+
+	#print("RESULTATS",resultats)
+	
+	return HTML
 
 @app.route("/app/<int:appid>")
 def app_detail(appid):
